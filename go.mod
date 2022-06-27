16d80520755e6aaaa01e7479cbb1f12f35d79597
aw4y <mobile@rehacktive.net>
2022-06-27 13:38:47 +0200

    add modules

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rehacktive/go-raspi-camera
+
+go 1.18
--- samples/sample.go
+++ samples/sample.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/rehacktive/go-raspi-camera"
+	camera "github.com/rehacktive/go-raspi-camera"
 )
 
 func main() {
